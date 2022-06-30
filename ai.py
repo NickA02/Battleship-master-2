783a4ef0942c213fb98f679f9d1aa6bfbb40d90e
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-06-30 16:30:23 +0200

    AI updates

--- ai.py
+++ ai.py
@@ -5,33 +5,32 @@ import matplotlib.pylab as plt
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 
-model = Sequential()
-model.add(Dense(200, input_dim=81, activation = 'relu'))
-model.add(Dense(81, activation='softmax'))
+model = keras.models.load_model('hunt_ai.h5')
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+"""board =[0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,2,0,2,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0]
 
-model.fit(x_train, y_train, batch_size=20, epochs=50, validation_split=0.1)
-model.summary()
-
-model.evaluate(x_test, y_test)
-
-best_guesses = np.zeros(81)
+best_guesses = model.predict(np.reshape(board, (1,81)))
 
 heat_map = np.reshape(best_guesses, (9, 9))
 
 fig, ax = plt.subplots(figsize=[10,10])
-im = ax.imshow(heat_map[:,:], cmap='gray', vmin=0, vmax=1)
-
-for i in range(9):
-    for j in range(9):
-        text = ax.text(j, i, np.format_float_positional(heat_map[i,j], precision=3),
-                       ha="center", va="center", color="blue")
+im = ax.imshow(np.reshape(board, (9,9)))
+plt.show()
+#im = ax.imshow(heat_map[:,:])
+plt.imshow(heat_map[:,:])"""
 
 
+#for i in range(9):
+#    for j in range(9):
+#        text = ax.text(j, i, np.format_float_positional(heat_map[i,j], precision=3),
+#                       ha="center", va="center", color="blue")
+#plt.colorbar()
+#plt.show()
 def model_predict(board):
+    print(board.shape)
+    board = np.array(board)
+    board = np.reshape(board, (1, 81))
+    model = keras.models.load_model('hunt_ai.h5')
     best_guesses = model.predict(board)
     new_best = np.reshape(best_guesses, (9,9))
     print(zip(*np.where(new_best == 1)))
