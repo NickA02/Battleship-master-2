e3b6726a580932517111bf27c89955ba9e089e43
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-06-30 23:39:26 +0200

    Created a redundancy fn almost ready for implement

--- plot.py
+++ plot.py
@@ -3,8 +3,8 @@ import numpy as np
 from tensorflow import keras
 
 def gen_heat_map(preds):
-    print(preds.shape)
-    print(preds)
+    """Generates Heat Map"""
+
     fig, ax = plt.subplots(figsize=[10,10])
     for i in range(9):
         for j in range(9):
@@ -12,3 +12,17 @@ def gen_heat_map(preds):
                         ha="center", va="center", color="blue")
     im = ax.imshow(preds[:,:]) 
     plt.show()
+
+def check_if_invalid_position(x,y,board):
+    board = np.reshape(board, (9,9))
+    if board[x][y] != 0:
+        print("Hit! Original guess: (" + str(x) + ', ' + str(y) + ')')
+        return True
+    return False
+
+    """    while plot.check_if_invalid_position(ret[0],ret[1],board):
+        new_best[ret[0]][ret[1]] = 0
+        ret = unravel_index(new_best.argmax(), new_best.shape)
+        
+        This is the insert for redundancy inspection on guesses
+    """
