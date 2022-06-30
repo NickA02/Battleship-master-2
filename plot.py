e61234b4b35f5854b74010b37cbab7f6714c678b
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-06-30 22:50:30 +0200

    Added plot function, all it needs to be inserted

--- plot.py
+++ plot.py
@@ -1,1 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+
+def gen_heat_map(preds):
+    print(preds.shape)
+    print(preds)
+    fig, ax = plt.subplots(figsize=[10,10])
+    for i in range(9):
+        for j in range(9):
+            text = ax.text(j, i, np.format_float_positional(preds[i][j], precision=3),
+                        ha="center", va="center", color="blue")
+    im = ax.imshow(preds[:,:]) 
+    plt.show()
